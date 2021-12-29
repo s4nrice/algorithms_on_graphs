818ab10dcbdf32c17b3568d7ab22d8cb319cb121
Антон <@DESKTOP-17B8U84>
2021-12-29 21:31:30 +0300

    Добавьте файлы проекта.

---
+++ algorithms on graphs/Graph.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algorithms_on_graphs
+{
+	 class Graph
+	 {
+			private List<List<char>> matrix = new List<List<char>>();
+			public int matrixLength;
+			private List<char> dictionary = new List<char>();
+
+			public Graph(int matrixSize, List<char> dictionary)
+			{
+				 matrixLength = matrixSize + 1;
+
+				 this.dictionary.AddRange(dictionary);
+
+				 matrix.Add(new List<char>());
+				 matrix[0].Add(' ');
+
+				 for (int i = 0; i < matrixLength - 1; i++)
+				 {
+						ADD_V(this.dictionary[i]);
+				 }
+			}
+
+			// FIRST(v) - возвращает индекс первой вершины, смежной с вершиной v.Если вершина v не имеет смежных вершин, то возвращается "нулевая" вершина .
+			public int FIRST(char v)
+			{
+				 for (int i = 1; i < matrixLength; i++)
+				 {
+						// Находим подходящую указанную вершину
+						if (matrix[i][0] == v)
+						{
+							 // Находим индекс первой смежной с указанной вершиной вершины
+							 for (int j = 1; j < matrixLength; j++)
+							 {
+									if (matrix[i][j] != '0')
+									{
+										 return j;
+									}
+							 }
+						}
+				 }
+				 return 0;
+			}
+
+			// NEXT(v, i)- возвращает индекс вершины, смежной с вершиной v,
+			// следующий за индексом i. Если i — это индекс последней вершины, смежной с вершиной v, то возвращается .
+			public int NEXT(char v, int i)
+			{
+				 for (int a = 1; a < matrixLength; a++)
+				 {
+						// Находим подходящую указанную вершину
+						if (matrix[a][0] == v)
+						{
+							 // Находим индекс вершины, смежной с указанной вершиной, идущей после i-того индекса
+							 for (int b = i + 1; b < matrixLength; b++)
+							 {
+									if (matrix[a][b] != '0')
+									{
+										 return b;
+									}
+							 }
+						}
+				 }
+				 return 0;
+			}
+
+			// VERTEX(v, i) - возвращает вершину с индексом i из множества вершин, смежных с v.
+			public char VERTEX(char v, int i)
+			{
+				 for (int a = 1; a < matrixLength; a++)
+				 {
+						// Находим подходящую указанную вершину
+						if (matrix[a][0] == v)
+						{
+							 // Находим вершину с индексом i из множества вершин, смежных с v
+							 for (int b = 1; b < matrixLength; b++)
+							 {
+									if (b == i)
+									{
+										 return matrix[b][0];
+									}
+							 }
+						}
+				 }
+				 return '0';
+			}
+
+			// ADD_V(<имя>,<метка, mark>) - добавить УЗЕЛ 
+			public void ADD_V(char name)
+			{
+				 for (int i = 0; i < matrix.Count; i++)
+				 {
+						if (i == 0) matrix[0].Add(name);
+						else matrix[i].Add('0');
+				 }
+
+				 matrix.Add(new List<char>());
+				 for (int i = 0; i < matrix.Count; i++)
+				 {
+						if (i == 0) matrix[matrix.Count - 1].Add(name);
+						else matrix[matrix.Count - 1].Add('0');
+				 }
+			}
+
+			// ADD_Е(v, w, c) - добавить ДУГУ (здесь c — вес, цена дуги (v,w))
+			public void ADD_Е(char v, char w, char c)
+			{
+				 int index_of_first_letter = 0;
+				 int index_of_second_letter = 0;
+
+				 for (int i = 1; i < matrixLength; i++)
+				 {
+						if (matrix[i][0] == v)
+						{
+							 index_of_first_letter = i;
+						}
+				 }
+
+				 for (int i = 1; i < matrixLength; i++)
+				 {
+						if (matrix[0][i] == w)
+						{
+							 index_of_second_letter = i;
+						}
+				 }
+
+				 matrix[index_of_first_letter][index_of_second_letter] = c;
+			}
+
+			// DEL_V(<имя>) - удалить УЗЕЛ
+			public void DEL_V(char name)
+			{
+				 //matrix.Remove(matrix.Find((array) => array[0] == name));
+
+				 for (int i = 1; i < matrixLength; i++)
+				 {
+						if (matrix[i][0] == name) {
+							 matrix.RemoveAt(i);
+
+							 for (int j = 0; j < matrixLength - 1; j++)
+							 {
+									matrix[j].RemoveAt(i);
+							 }
+							 matrixLength--;
+							 return;
+						}
+				 }
+			}
+
+			// DEL_Е(v, w) – удалить ДУГУ
+			public void DEL_Е(char v, char w)
+			{
+				 for (int i = 1; i < matrixLength; i++)
+				 {
+						for (int j = 1; j < matrixLength; j++)
+						{
+							 if (matrix[i][0] == v && matrix[0][j] == w)
+							 {
+									matrix[i][j] = '0';
+									return;
+							 }
+						}
+				 }
+			}
+
+			// EDIT_V(<имя>, <новое значение метки или маркировки>) - изменить метку (маркировку) УЗЛА
+			public void EDIT_V(char name, char new_name)
+			{
+				 for (int i = 1; i < matrixLength; i++)
+				 {
+						// Находим подходящую указанную вершину
+						if (matrix[i][0] == name)
+						{
+							 matrix[i][0] = new_name;
+							 matrix[0][i] = new_name;
+							 return;
+						}
+				 }
+			}
+
+			// EDIT_Е(v, w, <новый вес дуги>) - изменить вес ДУГИ
+			public void EDIT_Е(char v, char w, char newWeight)
+			{
+				 for (int i = 0; i < matrixLength; i++)
+				 {
+						for (int j = 0; j < matrixLength; j++)
+						{
+							 if (matrix[i][0] == v && matrix[0][j] == w)
+							 {
+									matrix[i][j] = newWeight;
+									return;
+							 }
+						}
+				 }
+			}
+
+			public void PrintGraph()
+			{
+				 for (int i = 0; i < matrixLength; i++)
+				 {
+						for (int j = 0; j < matrixLength; j++)
+						{
+							 Console.Write(matrix[i][j] + "    ");
+						}
+						Console.WriteLine("\n");
+				 }
+			}
+	 }
+}
---
+++ algorithms on graphs/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algorithms_on_graphs
+{
+	 class Program
+	 {
+			private static List<char> Names = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P' };
+			private static List<string> answers = new List<string>();
+			private static List<char> answer = new List<char>();
+			private static Graph graph;
+
+			public static void Main()
+			{
+				 // ГРАФ 1
+				 graph = new Graph(5, Names);
+				 
+				 graph.EDIT_Е('A', 'B', '6');
+				 graph.EDIT_Е('A', 'D', '7');
+				 graph.EDIT_Е('B', 'C', '2');
+				 graph.EDIT_Е('C', 'A', '3');
+				 graph.EDIT_Е('C', 'E', '8');
+				 graph.EDIT_Е('D', 'B', '2');
+				 graph.EDIT_Е('D', 'C', '5');
+				 graph.EDIT_Е('D', 'E', '9');
+				 graph.EDIT_Е('D', 'A', '8');
+				 graph.EDIT_Е('E', 'A', '8');
+
+				 // Вывод введенного графа на консоль
+				 graph.PrintGraph();
+
+				 // Нахождение всех простых путей для графа
+				 findPaths();
+
+				 // Вывод всех простых путей
+				 Console.WriteLine("\tВСЕ ПРОСТЫЕ ПУТИ ДЛЯ ВВЕДЕННОГО ГРАФА\n");
+				 for (int i = 0; i < answers.Count; i++)
+				 {
+						if (i != 0 && answers[i][0] != answers[i - 1][0])
+							 Console.WriteLine("\n");
+						Console.Write(answers[i] + ", ");
+				 }
+				 Console.ReadKey();
+			}
+
+			public static void findPaths()
+			{
+				 for (int i = 1; i < graph.matrixLength; i++)
+				 {
+						adjacentPaths(i, Names, answer);
+				 }
+			}
+
+			public static void adjacentPaths(int i, List<char> names, List<char> answer)
+			{
+				 char vertex = Names[i - 1];
+
+				 List<char> names1 = new List<char>();
+				 names1.AddRange(names);
+
+				 List<char> answer1 = new List<char>();
+				 answer1.AddRange(answer);
+
+				 answer1.Add(vertex);
+				 names1.Remove(vertex);
+
+				 int counter = 0;
+
+				 for (int j = graph.FIRST(vertex); j > 0; j = graph.NEXT(vertex, j))
+				 {
+						if (names1.Contains(graph.VERTEX(vertex, j)))
+						{
+							 counter ++;
+							 adjacentPaths(j, names1, answer1);
+						}
+				 }
+
+				 // Если все вершины, которые связаны с текущей уже были посещены, но текущая вершина считается последней
+				 if (counter == 0)
+				 {
+						string final_answer = "";
+						foreach (char letter in answer1)
+						{
+							 final_answer += letter;
+						}
+						answers.Add(final_answer);
+				 }
+			}
+	 }
+}
